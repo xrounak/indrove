9f9541cbdf48cdc205b9bb6f5a129b2dd983b209
xrounak <rounakkumar13005@gmail.com>
2025-07-10 16:11:09 +0530

    removing imp credntials :-{

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,28 +1,22 @@
-// Import the functions you need from the SDKs you need
+// src/services/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; // Import Firebase Authentication
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyChFOYeH4PgxT3Hg6OHI9-93FzqdXZA38w",
-  authDomain: "unknown-indrove.firebaseapp.com",
-  projectId: "unknown-indrove",
-  storageBucket: "unknown-indrove.firebasestorage.app",
-  messagingSenderId: "1086003844317",
-  appId: "1:1086003844317:web:bf5b5f90fbd6791c403734",
-  measurementId: "G-156CJ3N4B6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
